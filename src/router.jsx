a5ed697ac9d048988eda38fbcc484945f9b78337
sahildafda <20bmiit042@gmail.com>
2026-04-20 22:35:55 +0530

    By Default show page not found.

--- src/router.jsx
+++ src/router.jsx
@@ -4,7 +4,7 @@ import LovePage from './pages/LovePage';
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Navigate to="/sahil-kirti" replace />
+    element: <Navigate to="/page-not-found" replace />
   },
   {
     path: '/:coupleId',
@@ -12,6 +12,6 @@ export const router = createBrowserRouter([
   },
   {
     path: '*',
-    element: <Navigate to="/sahil-kirti" replace />
+    element: <Navigate to="/page-not-found" replace />
   }
 ]);
